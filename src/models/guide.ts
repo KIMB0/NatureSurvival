8222b02a7ef0e71233e2324b55069e06635c1173
KIMB0 <Kimsen1992@gmail.com>
2017-02-14 21:03:29 +0100

    Added model and icons
    Added the right varibles in model. Created a survivalList in home.ts and
    looped it ind home.html

---
+++ src/models/guide.ts
@@ -0,0 +1,13 @@
+export class Guide{
+
+  public title: string;
+  public description: string;
+  public image: string;
+
+  constructor(title, description, image){
+    this.title = title,
+    this.description = description,
+    this.image = image
+  }
+  
+}

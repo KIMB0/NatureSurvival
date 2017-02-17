d3150a714b6fee02edfd8b7c7774665cd8ba2e0f
KIMB0 <Kimsen1992@gmail.com>
2017-02-17 12:27:38 +0100

    Created new map for img
    Removed imgs' to that map

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,9 +16,9 @@ export class HomePage {
 
   ionViewDidEnter(){
     this.survivalList = []
-    this.survivalList.push(new Guide("Food","How to find food", "/assets/icon/food.png"))
-    this.survivalList.push(new Guide("Drinks","How to find drinks", "/assets/icon/drink.png"))
-    this.survivalList.push(new Guide("Shelter","How to make shelter", "/assets/icon/shelter.png"))
-    this.survivalList.push(new Guide("Tools","How to make tools", "/assets/icon/tool.png"))
+    this.survivalList.push(new Guide("Food","How to find food", "/assets/img/food.png"))
+    this.survivalList.push(new Guide("Drinks","How to find drinks", "/assets/img/drink.png"))
+    this.survivalList.push(new Guide("Shelter","How to make shelter", "/assets/img/shelter.png"))
+    this.survivalList.push(new Guide("Tools","How to make tools", "/assets/img/tool.png"))
   }
 }

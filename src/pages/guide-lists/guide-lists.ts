b9c7045f958e120bd988852944b6ee97e1f47788
KIMB0 <Kimsen1992@gmail.com>
2017-02-17 12:03:10 +0100

    Generated new page
    And removed auto-comments

---
+++ src/pages/guide-lists/guide-lists.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-guide-lists',
+  templateUrl: 'guide-lists.html'
+})
+export class GuideListsPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad GuideListsPage');
+  }
+
+}

4a3748744c7e151f2ed2a913e532534c4c869bbe
KIMB0 <Kimsen1992@gmail.com>
2017-02-20 17:45:39 +0100

    Implemented search function
    The list now save into localeStorage so the searchbar can use it.
    Also removed a log :-1:

--- src/pages/guide-lists/guide-lists.ts
+++ src/pages/guide-lists/guide-lists.ts
@@ -10,9 +10,33 @@ import { GuideDetailsPage } from '../guide-details/guide-details';
 export class GuideListsPage {
 
   public selectedGuide: any;
+  searchQuery: string = '';
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.selectedGuide = this.navParams.get("selectedGuide");
+    localStorage.setItem("guides", JSON.stringify(this.selectedGuide))
+    this.intializeItems();
+    this.searchQuery = "";
+  }
+
+  intializeItems(){
+    this.selectedGuide = JSON.parse(localStorage.getItem("guides"));
+  }
+
+  getItems(searchbar: any){
+    this.intializeItems();
+
+    let val = searchbar.target.value;
+
+    if(val && val.trim() != ""){
+
+    this.selectedGuide.guideItem = this.selectedGuide.guideItem.filter((item) => {
+      return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+    })
+   }
+  }
+  onClear(){
+    this.intializeItems();
   }
 
   goToGuideDetailsPage(guideItem){

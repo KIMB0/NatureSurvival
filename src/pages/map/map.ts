380fa92393e4926844a8c77497d1ac1e423ef0bf
KIMB0 <Kimsen1992@gmail.com>
2017-02-25 19:28:42 +0100

    Implemented Google Maps
    Now the user sees a map, and can get the current location.
    Also added styling to div

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-
+import { NavController, Platform } from 'ionic-angular';
+import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng } from 'ionic-native';
 
 @Component({
   selector: 'page-map',
@@ -8,9 +8,42 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class MapPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  map: GoogleMap;
 
-  ionViewDidLoad() {
+  constructor(public navCtrl: NavController, public platform: Platform) {
+    platform.ready().then(() => {
+      this.loadMap();
+    })
+
   }
 
+  loadMap() {
+    let location = new GoogleMapsLatLng(55.641910,12.087845);
+
+    this.map = new GoogleMap('map', {
+      'backgroundColor': 'white',
+      'controls': {
+        'compass': true,
+        'myLocationButton': true,
+        'indoorPicker': true,
+        'zoom': true
+      },
+      'gestures': {
+        'scroll': true,
+        'tilt': true,
+        'rotate': true,
+        'zoom': true
+      },
+      'camera': {
+        'latLng': location,
+        'tilt': 30,
+        'zoom': 15,
+        'bearing': 50
+      }
+    });
+
+    this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+      console.log("Map is ready!");
+    })
+  }
 }

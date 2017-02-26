970e1c496717feb06ac0d812e8aa22b61003f471
KIMB0 <Kimsen1992@gmail.com>
2017-02-26 23:07:22 +0100

    Created enableMyLocation method
    it checks if the togglebutton is true or false: on/off

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -9,41 +9,50 @@ import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng } from 'ionic-native';
 export class MapPage {
 
   map: GoogleMap;
+  public toggleMap: boolean = true;
 
   constructor(public navCtrl: NavController, public platform: Platform) {
     platform.ready().then(() => {
       this.loadMap();
     })
+  }
 
+  enableMyLocation(){
+    if(this.toggleMap == true){
+      this.map.setMyLocationEnabled(true);
+    }
+    else{
+      this.map.setMyLocationEnabled(false);
+    }
   }
 
   loadMap() {
-    let location = new GoogleMapsLatLng(55.641910,12.087845);
+      let location = new GoogleMapsLatLng(55.641910,12.087845);
 
-    this.map = new GoogleMap('map', {
-      'backgroundColor': 'white',
-      'controls': {
-        'compass': true,
-        'myLocationButton': true,
-        'indoorPicker': true,
-        'zoom': true
-      },
-      'gestures': {
-        'scroll': true,
-        'tilt': true,
-        'rotate': true,
-        'zoom': true
-      },
-      'camera': {
-        'latLng': location,
-        'tilt': 30,
-        'zoom': 15,
-        'bearing': 50
-      }
-    });
+      this.map = new GoogleMap('map', {
+        'backgroundColor': 'white',
+        'controls': {
+          'compass': true,
+          'myLocationButton': true,
+          'indoorPicker': true,
+          'zoom': true
+        },
+        'gestures': {
+          'scroll': true,
+          'tilt': true,
+          'rotate': true,
+          'zoom': true
+        },
+        'camera': {
+          'latLng': location,
+          'tilt': 30,
+          'zoom': 15,
+          'bearing': 50
+        }
+      });
 
-    this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
-      console.log("Map is ready!");
-    })
-  }
+      this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+        console.log("Map is ready!");
+      })
+    }
 }

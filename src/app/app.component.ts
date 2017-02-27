3c0a204738365857166e2c5eb3cd0b906bf647d6
KIMB0 <Kimsen1992@gmail.com>
2017-02-27 10:07:27 +0100

    Added a color to the Status Bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
+      StatusBar.backgroundColorByHexString("#1e1e1e");
       Splashscreen.hide();
     });
   }

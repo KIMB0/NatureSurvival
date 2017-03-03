70beb95b0c2d50d70412ae7a9cf2d2ba04f8c375
KIMB0 <Kimsen1992@gmail.com>
2017-03-03 15:44:39 +0100

    Binded data in weather.html
    so now the right informations are shown.
    Also added a boolean that is true when the observable is done. The
    boolean is set on the div.
    Also added som style classes, and added global styling.

--- src/pages/weather/weather.ts
+++ src/pages/weather/weather.ts
@@ -9,14 +9,16 @@ export class WeatherPage {
 
   private city: string;
   private weatherData: any;
+  private search: boolean = false;
 
   constructor(public weatherProvider: WeatherProvider) {
+
 
   }
 
   getCityWeather(){
     this.weatherProvider.getWeather(this.city).subscribe(data =>  { this.weatherData = data.json();
     },
-    err => alert(err), () => console.log(this.weatherData) );
+    err => alert(err), () => this.search = true );
   }
 }
